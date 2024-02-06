d90145c08bdcc8f280e5856a3f46963301e62512
Ayoub SAHMI <ayoubsahmi@gmail.com>
2024-02-05 17:29:04 -0800

    update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,29 +1,7 @@
 #!/usr/bin/python3
+""" module 0-lookup contains one function: lookup """
 
 
 def lookup(obj):
-            """Returns the list of available attributes and methods of an object.
-
-    Args:
-        obj (object): The object to inspect.
-
-    Returns:
-        list: List of attributes and methods.
-    """
-                return dir(obj)
-
-
-        if __name__ == "__main__":
-                    class MyClass1(object):
-                                    pass
-
-                                class MyClass2(object):
-                                                my_attr1 = 3
-
-                                                        def my_meth(self):
-                                                                            pass
-
-                                                                        print(lookup(MyClass1))
-                                                                            print(lookup(MyClass2))
-                                                                                print(lookup(int))
-                                                                                
+                """ returns a list of available attributes and methods of an object """
+                    return dir(obj)
